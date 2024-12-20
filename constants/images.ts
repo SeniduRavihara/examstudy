ebd8bef136c17144a2f478b0bbcf2956dae2d110
SeniduRavihara <senidumee16@gmail.com.com>
2024-12-20 21:22:26 +0530

    additional folders adds

---
+++ constants/images.ts
@@ -0,0 +1,16 @@
+// @ts-ignore
+import profile from "../assets/images/profile.png";
+// @ts-ignore
+import thumbnail from "../assets/images/thumbnail.png";
+// @ts-ignore
+import cards from "../assets/images/cards.png";
+// @ts-ignore
+import path from "../assets/images/path.png";
+// @ts-ignore
+import logo from "../assets/images/logo.png";
+// @ts-ignore
+import logoSmall from "../assets/images/logo-small.png";
+// @ts-ignore
+import empty from "../assets/images/empty.png";
+
+export default { profile, thumbnail, cards, path, logo, logoSmall, empty };
